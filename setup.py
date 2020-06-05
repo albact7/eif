dc20a07ee4a714d215464f4c2bb55b064d2afdee
albact7 <alba164@gmail.com>
2020-06-05 10:02:16 +0200

    fixing installation

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
                  language="c++")],
     scripts=[],
     py_modules=['version'],
-    packages=[],
+    packages=find_packages(),
     license='License.txt',
     include_package_data=True,
     description='Extended Isolation Forest for anomaly detection parallelized',
